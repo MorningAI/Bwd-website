6b66d5f1143fb6b031aaae5def9ae35fc1a3a8f1
Walid-Abbes <walid.abbes@etu.usthb.dz>
2022-09-25 17:59:40 +0100

    repo clean up p2

--- texap/pages/index.js
+++ texap/pages/index.js
@@ -5,7 +5,6 @@ import AboutUsArea from '@/components/HomeDemo5/AboutUsArea'
 import Features from '@/components/HomeDemo5/Features'
 import KeyFeatures from '@/components/HomeDemo5/KeyFeatures'
 import AppScreenshotsStyle5 from '@/components/AppScreenshots/AppScreenshotsStyle5'
-import PricingPlanStyle5 from '@/components/PricingPlan/PricingPlanStyle5'
 import AppDownloadStyle2 from '@/components/Common/AppDownloadStyle2'
 import ClientFeedbackStyle5 from '@/components/Feedbacks/ClientFeedbackStyle5'
 import FreeTrialStyle3 from '@/components/Common/FreeTrialStyle3'
@@ -13,7 +12,6 @@ import FooterStyleTwo from '@/components/_App/FooterStyleTwo'
 import AppScreenshotsStyle1 from '@/components/AppScreenshots/AppScreenshotsStyle1'
 import AppScreenshotsStyle3 from '@/components/AppScreenshots/AppScreenshotsStyle3'
 import AppProgressStyle1 from '@/components/Common/AppProgressStyle1'
-
 import AppProgressStyle4 from '@/components/Common/AppProgressStyle4'
 
  
@@ -33,8 +31,6 @@ const IndexPage5 = () => {
             <AppScreenshotsStyle1 />
             <AppScreenshotsStyle3 />
             <AppScreenshotsStyle5 />
-
-            <PricingPlanStyle5 />
             
             <div className="pt-100">
                 <AppDownloadStyle2 />
